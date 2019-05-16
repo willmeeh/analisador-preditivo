1fd782a8b37ab55c9a397d9ed13a137e94ece128
arthurhoch <arthur.j.h96@gmail.com>
2019-05-15 23:29:06 -0300

    Adicionar follow e veficador-sentenca

--- src/services/follow.js
+++ src/services/follow.js
@@ -1,83 +1,181 @@
-const START = 'start';
-const MIDDLE = 'middle';
-const END = 'end';
+const d7 = {
+    grammar: {
+        E: [
+            ['T', 'EL']
+        ],
+        EL: [
+            ['+', 'T', 'EL'],
+            ['empty']
+        ],
+        T: [
+            ['a']
+        ]
+    },
+    firsts: {
+        E: ['a'],
+        EL: ['+', 'empty'],
+        T: ['a']
+    }
+};
 
 const isTerminal = (symbol) => {
     return !/[A-Z]/g.test(symbol);
 }
 
-const getaBb = (symbol) =>{
-    const aBb = {
+const follow = (terminal, grammar, firsts, follows) => {
+
+    let resultado = [];
+
+    for (const prod in grammar) {
+        if (prod !== terminal) {
+            // eslint-disable-next-line no-loop-func
+            let entrou = false;
+            grammar[prod].forEach((sentencas) => {
+                let location = sentencas.indexOf(terminal);
+                if (location !== -1) {
+
+                    for (let i = location; i < sentencas.length; i++) {
+                        if (isTerminal(sentencas[i])) {
+                            entrou = true;
+                            resultado.push(sentencas[i]);
+                            break;
+                        }
+                    }
+                    if (entrou === false) {
+                        for (let i = location; i >= 0; i--) {
+                            if (isTerminal(sentencas[i])) {
+                                entrou = true;
+                                resultado.push(sentencas[i]);
+                                break;
+                            }
+                        }
+                    }
+                    if (entrou === false) {
+                        for (let i = location; i >= 0; i--) {
+                            if (!isTerminal(sentencas[i])) {
+                                entrou = true;
+                                resultado.push(...follows[prod]);
+                                break;
+                            }
+                        }
+                    }
+                    if (entrou === false) {
+                        for (let i = location; i < sentencas.length; i++) {
+                            if (!isTerminal(sentencas[i])) {
+                                entrou = true;
+                                resultado.push(...firsts[sentencas[i]]);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+
+            });
+        }
     }
-
-    var state = START;
-
-    let letterArray =symbol.split("");
-
-    letterArray.forEach(letter => {
-        if (letter === letter.toLowerCase()) {
-            if (state === MIDDLE) {
-                state = END;
-            } 
-            aBb[state] += letter;
-        }
-        else {
-            state = MIDDLE;
-            aBb[state] += letter;
-        }
-    });
-
-    return aBb;
+    return resultado.filter(function (item) {
+        return item !== 'empty'
+    })
 }
 
-const follow = (symbols, termial) => {
-    const aBb = getaBb(symbols);
+const getFollows = (grammar, firsts) => {
 
-    if (isTerminal(symbols)) {
-        // Adicionar terminal na lista
-        return {
-            termial: symbols
-        }
-    }
-
-    if (aBb[START] && aBb[MIDDLE] && aBb[END] === undefined) {
-        // Adicionar Follow de aBb[MIDDLE] na lista 
-        return {
-            follow: aBb[MIDDLE]
-        }
-    }
-
-    if (aBb[END] && aBb[MIDDLE] === termial) {
-        // Adicionar terminal na lista
-        return {
-            termial: aBb[END]
-        }
-    }
-
-    return undefined;
-}
-
-const getFollows = (jsonInput) => {
-    let follows = {};
     let first = true;
-    let result = undefined;
 
-    for (const key in jsonInput) {
-        const prod = jsonInput[key];
+    let follows = {
 
-        follows[key] = [];
+    }
 
-        if (first) {
-            follows[key].push('$');
+    for (const prod in grammar) {
+        if (!follows[prod]) {
+            follows[prod] = [];
+        }
+
+        follows[prod] = follow(prod, grammar, firsts, follows);
+
+        if (first === true) {
+            follows[prod].push('$');
             first = false;
-
-        } else {
-            result = follow(prod, key);
-            if (result) {
-                follows[key].push(result);
-            }
         }
     }
 
     return follows;
 }
+
+
+
+console.log(getFollows(d7.grammar, d7.firsts));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const follow = (symbols, termial) => {
+//     const aBb = getaBb(symbols);
+
+//     if (isTerminal(symbols)) {
+//         // Adicionar terminal na lista
+//         return {
+//             termial: symbols
+//         }
+//     }
+
+//     if (aBb[START] && aBb[MIDDLE] && aBb[END] === undefined) {
+//         // Adicionar Follow de aBb[MIDDLE] na lista 
+//         return {
+//             follow: aBb[MIDDLE]
+//         }
+//     }
+
+//     if (aBb[END] && aBb[MIDDLE] === termial) {
+//         // Adicionar terminal na lista
+//         return {
+//             termial: aBb[END]
+//         }
+//     }
+
+//     return undefined;
+// }
+
+// const getFollows = (jsonInput) => {
+//     let follows = {};
+//     let first = true;
+//     let result = undefined;
+
+//     for (const key in jsonInput) {
+//         const prod = jsonInput[key];
+
+//         follows[key] = [];
+
+//         if (first) {
+//             follows[key].push('$');
+//             first = false;
+
+//         } else {
+//             result = follow(prod, key);
+//             if (result) {
+//                 follows[key].push(result);
+//             }
+//         }
+//     }
+
+//     return follows;
+// }
---
+++ src/services/verificador-sentenca.js
@@ -0,0 +1,72 @@
+import { arthur_d1 } from '../data-sets';
+
+
+const escreverTabelafinal = (tabelaFinal) => {
+
+    let tamanho = tabelaFinal.pilha.length;
+
+    console.log("pilha \t\t", "entrada \t\t");
+
+    for (let index = 0; index < tamanho; index++) {
+        console.log("Linha: ", index);
+        console.log("Pilha: ", JSON.stringify(tabelaFinal.pilha[index]));
+        console.log("Entrada: ", JSON.stringify(tabelaFinal.entrada[index]));
+        console.log("Saída: ", JSON.stringify(tabelaFinal.saida[index]));
+        console.log("\n");
+
+    }
+}
+
+const verificarSentenca = (pilha, entrada, tabelaPreditivaTabular) => {
+    let tabelaFinal = {
+        pilha: [],
+        entrada: [],
+        saida: []
+    }
+
+    tabelaFinal.pilha.push([...pilha]);
+    tabelaFinal.entrada.push([...entrada]);
+    tabelaFinal.saida.push([]);
+
+    let encontrouNaTabela = true;
+
+    while (encontrouNaTabela) {
+        encontrouNaTabela = false;
+
+        let letraEntrada = entrada.shift();
+        let topoPilha = pilha.pop();
+
+        if (letraEntrada === topoPilha && letraEntrada !== '$') {
+            tabelaFinal.pilha.push([...pilha]);
+            tabelaFinal.entrada.push([...entrada]);
+            tabelaFinal.saida.push([]);
+            encontrouNaTabela = true;
+        } else {
+            // Percore as nao terminais
+            for (const naoTerminalTabela in tabelaPreditivaTabular) {
+                if (topoPilha === naoTerminalTabela) {
+                    for (const letraTabela in tabelaPreditivaTabular[topoPilha]) {
+                        if (letraEntrada === letraTabela) {
+                            entrada.unshift(letraEntrada);
+                            let tabelaPreditivaTabularTmp = [...tabelaPreditivaTabular[topoPilha][letraEntrada]];
+                            pilha.push(...tabelaPreditivaTabularTmp.reverse());
+                            pilha = pilha.filter(e => e !== 'empty');
+                            tabelaFinal.pilha.push([...pilha]);
+                            tabelaFinal.entrada.push([...entrada]);
+
+                            let sentenca = {};
+                            sentenca[topoPilha] = [...tabelaPreditivaTabularTmp];
+
+                            tabelaFinal.saida.push(sentenca);
+                            encontrouNaTabela = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    escreverTabelafinal(tabelaFinal);
+}
+
+
+// verificarSentenca(["$", "E"], ["a", "+", "a", "+", "a", "$"], arthur_d1);
